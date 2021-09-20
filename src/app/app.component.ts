28b29a1c19bfc3ba69ed2dfb14c55443b43f930d
Rahul Dutt <d92.rahul@gmail.com>
2021-09-20 12:05:13 +1200

    Initial Commit with Reporting Viewer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-demo-app';
+
+  reportServer: string = 'http://192.168.8.104:8091/reportserver';
+  reportUrl: string = 'ReceiptingInsuranceSystemReports/MonthlyPlatinumInsurance';
+  showParameters: string = "true";
+  parameters: any = {
+    // "SampleStringParameter": null,
+    // "SampleBooleanParameter" : false,
+    "Month" : "9/2020",
+    // "SampleDateTimeParameter" : "11/1/2020",
+    // "SampleIntParameter" : 1,
+    // "SampleFloatParameter" : "123.1234",
+    // "SampleMultipleStringParameter": ["Parameter1", "Parameter2"]
+  };
+  language: string = "en-us";
+  width: number = 100;
+  height: number = 100;
+  toolbar: string = "true";
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReportViewerModule } from 'ngx-ssrs-reportviewer';
 
 @NgModule({
   declarations: [
@@ -10,9 +11,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReportViewerModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
